e02f39483eb23dbb8754f13c5c8a53e62f29908c
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-21 12:45:21 -0400

    creating the models and associations

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Blog = require('./Blog');
+
+User.hasMany(Blog, {
+  foreignKey: 'user_name',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'user_name'
+});
+
+module.exports = { User, Blog };
