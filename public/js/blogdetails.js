beb67ddf7558dab715c6213434ce2f8be2494821
dassoumik <74944139+dassoumik@users.noreply.github.com>
2021-03-26 21:39:38 -0400

    added delete blog logic in individual blog detail page

--- public/js/blogdetails.js
+++ public/js/blogdetails.js
@@ -1,1 +1,20 @@
-console.log("in blogdet.js")
+const delButtonHandler = async (event) => {
+    console.log("in del event");
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+      console.log(id);
+      const response = await fetch(`/api/profile/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/api/profile');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+
+
+
+$(document).on('click', delButtonHandler);
